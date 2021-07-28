aca49293b83627effd7d74d52798a1fb8450d94b
Michael Bond <codemonkey8510@icloud.com>
2021-07-27 20:39:40 -0400

    Finish up game logic

---
+++ FourMagicNumberGame/Components/Game.razor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace FourMagicNumberGame.Components
+{
+    public partial class Game
+    {
+        [Parameter] public int Min { get; set; }
+
+        [Parameter] public int Max { get; set; }
+
+        private int? numberChosen;
+
+        List<string> outputLines = new();
+
+        private void DoTheMagic()
+        {
+            if (numberChosen == null)
+            {
+                return;
+            }
+            outputLines.Clear();
+            int currentLength = NumbersToWords.ConvertToWords(numberChosen ?? 0).Length;
+            Console.WriteLine(currentLength);
+            if (currentLength == 4)
+            {
+                outputLines.Add($"{numberChosen} is {currentLength}");
+                Console.WriteLine($"{numberChosen} is {currentLength}");
+            }
+            else
+            {
+                do
+                {
+                    outputLines.Add($"{numberChosen} is {currentLength}");
+                    Console.WriteLine($"{numberChosen} is {currentLength}");
+                    numberChosen = currentLength;
+                    currentLength = NumbersToWords.ConvertToWords(numberChosen ?? 0).Length;
+                }
+                while (currentLength != 4);
+                outputLines.Add($"{numberChosen} is {currentLength}");
+                Console.WriteLine($"{numberChosen} is {currentLength}");
+            }
+        }
+    }
+}
