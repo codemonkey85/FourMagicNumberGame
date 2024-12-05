1613de357b31c4abffcc8f5e2ab2415adb966e77
codemonkey85 <michael@bondcodes.com>
2024-12-05 18:30:41 +0000

    Deploying to gh-pages from @ codemonkey85/FourMagicNumberGame@2c786cba3cd358c547d15e604210cd1504b2fdfa 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: uvgjMiHN */
+/* Manifest version: mpIe5u0Y */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
